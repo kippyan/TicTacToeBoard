47211f48907fe414c74c7b73f0a290c71af122bc
Olivia Lund <recursory@Olivias-MacBook-Air.local>
2019-11-22 20:26:52 -0800

    add getWinner

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -2,7 +2,7 @@
 /**
  * Class for representing a 3x3 Tic-Tac-Toe game board, using the Piece enum
  * to represent the spaces on the board.
-**/
+ **/
 
 //Constructor sets an empty board and specifies it is X's turn first
 TicTacToeBoard::TicTacToeBoard()
@@ -16,7 +16,7 @@ TicTacToeBoard::TicTacToeBoard()
 /**
  * Switches turn member variable to represent whether it's X's or O's turn
  * and returns whose turn it is
-**/
+ **/
 Piece TicTacToeBoard::toggleTurn()
 {
   if(turn == O)
@@ -34,7 +34,7 @@ Piece TicTacToeBoard::toggleTurn()
  * Out of bounds coordinates return the Piece Invalid value. When the game
  * is over, no more pieces can be placed so attempting to place a piece
  * should neither change the board nor change whose turn it is.
-**/ 
+ **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
   if(row < 0 || row >= BOARDSIZE || column < 0 || column >= BOARDSIZE)
@@ -49,7 +49,7 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
   if(row < 0 || row >= BOARDSIZE || column < 0 || column >= BOARDSIZE)
@@ -60,8 +60,23 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won.
-**/
+ **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+  int moveCount = 0;
+  for(int i=0; i<BOARDSIZE; i++)
+    for(int j=0; j<BOARDSIZE; j++)
+      if(board[i][j] == Blank)
+        return Invalid;
+  if(board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    return board[0][0];
+  if(board[2][0] == board[1][1] && board[1][1] == board[0][2])
+    return board[2][0];
+  for(int i = 0; i < BOARDSIZE; i++)
+    if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
+      return board[0][i];
+  for(int i = 0; i < BOARDSIZE; i++)
+    if(board[i][0] == board[i][1] && board[i][1] == board[i][2])
+      return board[i][0];
+  return Blank;
 }
